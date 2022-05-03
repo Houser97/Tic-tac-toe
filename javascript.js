aa5c82511ab35f354ac69f61d633298d25f2ec66
Arturo Rivera <arturo.riverar97@gmail.com>
2022-05-02 20:46:16 -0500

    Fix bugs when the square is already occupied
    The first bug came about when the div for the circle was introduced, as
    the event listener took those DIVS to insert another circle. It was
    fixed by adding three 'empty' classes to it as soon as it is created,
    that way it not longer met the condition that the number of classes must
    be less than 3, so the instruction was ignored.
    
    On the other hand, when a square was already occupied but the user kept
    on clicking on it, it was possible to make 2 consecutive x. This was
    fixed by removing the variable currentTurn from the game object and
    placing it withing the player object. As soon as the putXorO is called,
    this variable is increased by 1, but if none of the conditions were met,
    then the variable decreased by 1 to allow the current player to click on
    other DIV. Likewise, when the second player made her or his move, the
    variable was set to 1 again to continue the cycle.

--- javascript.js
+++ javascript.js
@@ -19,16 +19,25 @@ const gameBoard = ((doc) => {
 })(document);
 
 const Players = (name, symbol)=>{
-    let putXorO = (e) => {      
-        if(symbol === '+' && e.target.classList.length < 3){
-                    e.target.classList.add('selectedX')
-                    e.target.textContent='+';
-                } else if(symbol === 'o' && e.target.classList.length < 3){
-                    let div = document.createElement('div');
-                    div.classList.add('selectedO')
-                    e.target.appendChild(div)
-                }
-        };
+    let putXorO = (e, currentTurn) => { 
+        currentTurn += 1;     
+        if(symbol === '+' && e.target.classList.length <= 2){
+            e.target.classList.add('pivot')
+            e.target.classList.add('selectedX')
+            e.target.textContent='+';
+        } else if(symbol === 'o' && e.target.classList.length <= 2){
+            e.target.classList.add('pivot')
+            let div = document.createElement('div');
+            div.classList.add('pivot');
+            div.classList.add('pivot1');
+            div.classList.add('selectedO');
+            e.target.appendChild(div);
+            currentTurn = 1;
+        } else {
+           currentTurn -= 1;
+        }
+        return currentTurn;
+    };
     return {putXorO};
 };
 
@@ -42,11 +51,9 @@ const newGame = (currentTurn)=>{
     const getCurrentTurn = () => currentTurn;
     const playerToPlay = (e) => {
         if(currentTurn == 1) {
-            player1.putXorO(e);
-            currentTurn += 1;
-        } else {
-            player2.putXorO(e);
-            currentTurn = 1;
+            currentTurn = player1.putXorO(e, currentTurn);
+        } else if (currentTurn == 2){
+            currentTurn = player2.putXorO(e, currentTurn);
         } 
     };
     return {playerToPlay}
